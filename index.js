6380674f9eb94469bcb8f1785afe9f24b788a0fa
shitoujianzibu <love35334213@163.com>
2017-11-23 11:37:10 +0800

    Update index.js
    如果没有meta标签，添加meta

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function (win, doc) {
     var docEl = doc.documentElement;
-
+    
     function setRemUnit() {
         var dpr, scale;
         if (win.devicePixelRatio) {
@@ -10,18 +10,24 @@
             dpr = 1;
             scale = 1 / dpr;
         }
+
         // 先缩放
         var metaEl = doc.querySelector('meta[name = "viewport"]');
-        metaEl.setAttribute('content', "initial-scale = " + scale + ", minimum-scale = " + scale + ", maximum-scale = " + scale);
+        if(!metaEl){
+            metaEl = doc.createElement('meta');
+            doc.head.appendChild(metaEl);
+        }
+        metaEl.setAttribute('name', "viewport");
+        metaEl.setAttribute('content', "width = device-width, user-scalable = no, initial-scale = " + scale + ", minimum-scale = " + scale + ", maximum-scale = " + scale);
         var docWidth = docEl.clientWidth;
-        console.log(docEl.clientWidth);
+
         // 调整页面缩放
-        var rem = docWidth / 16;
+        var rem = (docWidth / 750) * 100;
         docEl.style.fontSize = rem + 'px';
     }
 
     win.addEventListener('resize', function () {
-        setRemUnit();
+        //setRemUnit();
     }, false);
     // 页面从缓存中读取
     win.addEventListener('pageshow', function (e) {
@@ -31,5 +37,4 @@
     });
 
     setRemUnit();
-
 })(window, document);
